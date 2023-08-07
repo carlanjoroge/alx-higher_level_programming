46925d63bfe817559bbe4277bd0826799ae83f3d
carlanjoroge <carlanjoroge16@gmail.com>
2023-08-07 07:14:11 -0700

    Easter egg

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,13 +1,2 @@
 #!/usr/bin/python3
-zen_of_python = """
-The Zen of Python, by Tim Peters
-
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-...
-"""
-
-print(zen_of_python)
+import this

51b7a7c319581c5baa73523cfc6de1ac0c384574
carlanjoroge <carlanjoroge16@gmail.com>
2023-08-14 10:34:08 -0700

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+
 def element_at(_list, idx):
-    return(_list[idx] if 0 <= idx < len(_list) else "None")
+ if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])

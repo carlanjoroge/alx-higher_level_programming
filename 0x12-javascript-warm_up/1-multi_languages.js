14b3cb0db8e7aa39d89d46aa1c0b8b96e42c6f70
carlanjoroge <carlanjoroge16@gmail.com>
2023-11-14 21:54:34 +0300

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+
+const firstline = 'C is fun';
+const secondline = 'Python is cool';
+const thirdline = 'JavaScript is amazing';
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
